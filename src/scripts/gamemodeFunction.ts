a65b987e291cf821fffbab351fe699c66a9c6097
Absolute Phoenix <absolutephoenix2017@gmail.com>
2024-10-28 00:53:45 -0400

    updated git ignore

---
+++ src/scripts/gamemodeFunction.ts
@@ -0,0 +1,58 @@
+function phoenix_openGamemodeInterface(interfaceName: string): void {
+    const gamemodeSelections = document.getElementsByTagName(
+        "gamemode-selection"
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    const dispLTE = mod
+        .getContext("phoenixgamemodemanager")
+        .accountStorage.getItem("phoenix-display-lte") as string | null;
+
+    Array.from(gamemodeSelections).forEach((element: HTMLElement) :void => {
+        if (element.classList.contains("phoenix-cat-" + interfaceName)) {
+            element.style.display = "";
+        }
+
+        if (dispLTE === undefined || !dispLTE) return;
+
+        if (
+            interfaceName === "other" &&
+            element.classList.contains("phoenix-cat-lte")
+        ) {
+            element.style.display = "";
+        }
+    });
+
+    const elements : NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(
+        "#phoenix-category-header, #phoenix-category-container, #phoenix-gamemode-header, #phoenix-gamemode-container"
+    );
+
+    elements.forEach((element : HTMLElement) : void => {
+        if (element.id.startsWith("phoenix-category")) {
+            element.style.display = "none";
+        } else {
+            element.style.display = "";
+        }
+    });
+}
+
+function phoenix_openStyleInterface(): void {
+    const gamemodeSelections = document.getElementsByTagName(
+        "gamemode-selection"
+    ) as HTMLCollectionOf<HTMLElement>;
+
+    for (const element of gamemodeSelections) {
+        element.style.display = "none";
+    }
+
+    const elements : NodeListOf<HTMLElement> = document.querySelectorAll<HTMLElement>(
+        "#phoenix-category-header, #phoenix-category-container, #phoenix-gamemode-header, #phoenix-gamemode-container"
+    );
+
+    elements.forEach((element : HTMLElement) => {
+        if (element.id.startsWith("phoenix-category")) {
+            element.style.display = "";
+        } else {
+            element.style.display = "none";
+        }
+    });
+}
